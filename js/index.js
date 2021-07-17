219bb063b513a0877cddeab347f83467d301ce7e
Shikai1997 <56082441+Shikai1997@users.noreply.github.com>
2021-07-17 22:14:34 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,63 @@
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            activeData: 'D1',
+            drawDown: false,
+            worksData: {
+                D1: [
+                    './images/works/A-1.jpeg',
+                    './images/works/A-2.jpeg',
+                    './images/works/A-3.jpeg',
+                    './images/works/A-4.jpeg',
+                ],
+                D2: [
+                    './images/works/B-1.jpeg',
+                    './images/works/B-2.jpeg',
+                    './images/works/B-3.jpeg',
+                    './images/works/B-4.jpeg',
+                    './images/works/B-5.jpeg',
+                    './images/works/B-6.jpeg',
+                ],
+                D3: [
+                    './images/works/C-1.jpeg',
+                    './images/works/C-2.jpeg',
+                    './images/works/C-3.jpeg',
+                    './images/works/C-4.jpeg',
+                    './images/works/C-5.jpeg',
+                    './images/works/C-6.jpeg',
+                    './images/works/C-7.jpeg',
+                ],
+            }
+        }
+    },
+    methods: {
+        scrollPop(dom) {
+            const domOffset = this.$refs[dom].offsetTop || 0
+            window.scrollTo({
+                top: domOffset,
+                behavior: 'smooth'
+            });
+            if (window.innerWidth <= 768) {
+                this.drawDown = false
+            }
+        },
+        selectWorks(data) {
+            this.activeData = data
+            this.$refs.works_main.scrollTop = 0
+        },
+        drawDownNav() {
+            this.drawDown = !this.drawDown
+            console.log(this.drawDown)
+            return
+        },
+    },
+    computed: {
+        activeWork() {
+            return this.worksData[this.activeData]
+        }
+    },
+    mounted() {
+        console.log('init')
+    },
+})
